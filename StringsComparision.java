6051c142038c77800f2d69e8b7617818f9468071
Vamsi2100 <Vamsifighter2108@gmail.com>
2023-05-16 12:50:55 +0530

    Added: StringsComparision file

---
+++ StringsComparision.java
@@ -0,0 +1,13 @@
+public class StringsComparision {
+    public static void main(String[] args) {
+        String s1 = "Welcome to java!";
+        String s2 = "Welcome to java!";
+
+        if (s1.equals(s2)) {
+            System.out.println("The two strings are equal");
+        }
+        else {
+            System.out.println("The two strings are not equal");
+        }
+    }
+}
