d1fcd9408efb8ee93bd94eb99b45b61676e19221
Vamsi2100 <Vamsifighter2108@gmail.com>
2023-05-16 18:34:42 +0530

    Added: Static.java file

--- Static.java
+++ Static.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Static {
+public class Static {
+   static int staticVariable = 10;
+
+   static {
+       System.out.println("This is static bloccleak");
+       staticVariable = 20;
+   }
+   static void staticMethod(){
+       System.out.println("This is static method");
+       System.out.println("Static variable value:" + staticVariable);
+   }
+
+    public static void main(String[] args) {
+        System.out.println("Static variable value: " + staticVariable);
+        staticMethod();
+    }
 }
