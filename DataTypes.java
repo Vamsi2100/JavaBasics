89a8a356f7d7ab5cdc5f852ab5a20567207f5bbc
Vamsi2100 <Vamsifighter2108@gmail.com>
2023-05-16 12:49:39 +0530

    Added: Datatype file

---
+++ DataTypes.java
@@ -0,0 +1,21 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        byte b = 0;
+        short s = 0;
+        int a = 0;
+        long l = 0L;
+        boolean B = false;
+        char c = '\u0000';
+        double d = 0.0d;
+        float f = 0.0f;
+        System.out.println("byte : " + b);
+        System.out.println("short : " + s);
+        System.out.println("int : " + a);
+        System.out.println("long : " + l);
+        System.out.println("boolean : " + B);
+        System.out.println("char : " + c);
+        System.out.println("double : " + d);
+        System.out.println("float : " + f);
+
+    }
+}
